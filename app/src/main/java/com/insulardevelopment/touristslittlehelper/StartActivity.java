5a80b4a32b023c0a28b39da6e1b30a55adef643a
baglai <marina_95baglai@mail.ru>
2016-11-29 01:14:34 +0300

    recyclerview для списка типов мест

--- app/src/main/java/com/insulardevelopment/touristslittlehelper/StartActivity.java
+++ app/src/main/java/com/insulardevelopment/touristslittlehelper/StartActivity.java
@@ -2,10 +2,19 @@ package com.insulardevelopment.touristslittlehelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.insulardevelopment.touristslittlehelper.placetype.PlacesTypes;
+import com.insulardevelopment.touristslittlehelper.placetype.PlacesTypesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 *   Активити для выбора необходимых типов мест при первом запуске приложения
@@ -13,7 +22,6 @@ import android.widget.ListView;
 
 public class StartActivity extends AppCompatActivity {
 
-    private ListView typesList;
     private Button confirmBtn;
 
     @Override
@@ -21,15 +29,28 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        typesList = (ListView) findViewById(R.id.places_types_list_view);
-        String[] types = new String[]{"Museum", "Theatre", "Art Gallery", "Park", "Casino", "Zoo"};
-        ArrayAdapter<String> typesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, types);
-        typesList.setAdapter(typesAdapter);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.places_types_recycler_view);
+        final List<PlacesTypes> placesTypes = new ArrayList<>();
+
+        placesTypes.add(0, new PlacesTypes("Museum"));
+        placesTypes.add(0, new PlacesTypes("Theatre"));
+        placesTypes.add(0, new PlacesTypes("Art Gallery"));
+        placesTypes.add(0, new PlacesTypes("Park"));
+        placesTypes.add(0, new PlacesTypes("Casino"));
+        placesTypes.add(0, new PlacesTypes("Zoo"));
+
+        final PlacesTypesAdapter placesTypesAdapter = new PlacesTypesAdapter(placesTypes);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(placesTypesAdapter);
 
         confirmBtn = (Button) findViewById(R.id.start_confirm_btn);
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                boolean[] checked = placesTypesAdapter.getChecked();
                 MainActivity.start(StartActivity.this);
             }
         });
---
+++ app/src/main/java/com/insulardevelopment/touristslittlehelper/placetype/PlacesTypes.java
@@ -0,0 +1,24 @@
+package com.insulardevelopment.touristslittlehelper.placetype;
+
+/**
+ * Created by админ on 28.11.2016.
+ */
+
+public class PlacesTypes {
+    private String typesName;
+
+    public PlacesTypes() {
+    }
+
+    public PlacesTypes(String typesName) {
+        this.typesName = typesName;
+    }
+
+    public String getTypesName() {
+        return typesName;
+    }
+
+    public void setTypesName(String typesName) {
+        this.typesName = typesName;
+    }
+}
