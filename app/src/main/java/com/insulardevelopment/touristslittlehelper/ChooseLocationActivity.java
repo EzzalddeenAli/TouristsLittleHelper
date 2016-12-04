a7484c1dfe62202cc8774ca97ad3fe9344e490b8
baglai <marina_95baglai@mail.ru>
2016-12-04 12:20:22 +0300

    маркер местоположения

--- app/src/main/java/com/insulardevelopment/touristslittlehelper/ChooseLocationActivity.java
+++ app/src/main/java/com/insulardevelopment/touristslittlehelper/ChooseLocationActivity.java
@@ -2,11 +2,22 @@ package com.insulardevelopment.touristslittlehelper;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,12 +25,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 /*
 *   Активити для выбора города
 */
 
-public class ChooseLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+public class ChooseLocationActivity extends FragmentActivity implements OnMapReadyCallback,  GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
     private GoogleMap mMap;
     private Button nextButton;
 
@@ -39,11 +56,24 @@ public class ChooseLocationActivity extends FragmentActivity implements OnMapRea
                 PopularPlacesActivity.start(ChooseLocationActivity.this);
             }
         });
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+        }
     }
 
     public static void start(Context context){
         Intent intent = new Intent(context, ChooseLocationActivity.class);
         context.startActivity(intent);
+
     }
 
 
@@ -62,9 +92,36 @@ public class ChooseLocationActivity extends FragmentActivity implements OnMapRea
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+                List<Address> addresses = gcd.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+                if (addresses.size() > 0)
+                    setTitle(addresses.get(0).getAddressLine(1));
+            }
+            LatLng sydney = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        } catch (SecurityException e) {
+        }catch (Exception e) {
+            String s = e.getMessage();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
 }
