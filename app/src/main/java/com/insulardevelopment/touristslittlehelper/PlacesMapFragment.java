dff14bd42b8e1dc6bdf78a61e19c980ca704ba5a
Sabelnikova <mynameismargarita@gmail.com>
2017-02-18 21:30:33 +0300

    Попытка сделать работающий CheckBox в InfoWindow

--- app/src/main/java/com/insulardevelopment/touristslittlehelper/PlacesMapFragment.java
+++ app/src/main/java/com/insulardevelopment/touristslittlehelper/PlacesMapFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +19,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.insulardevelopment.touristslittlehelper.place.Place;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,11 +31,62 @@ public class PlacesMapFragment extends Fragment implements OnMapReadyCallback, G
     private GoogleMap mMap;
     private LatLng selectedLatLng;
     private List<Place> places;
+    private List<PlaceInfo> placeInfos;
+
+    class PlaceInfo{
+        private Place place;
+        private Marker marker;
+        private View view;
+        private boolean chosen = true;
+
+        public PlaceInfo() {
+        }
+
+        public PlaceInfo(Place place) {
+            this.place = place;
+        }
+
+        public Place getPlace() {
+            return place;
+        }
+
+        public void setPlace(Place place) {
+            this.place = place;
+        }
+
+        public Marker getMarker() {
+            return marker;
+        }
+
+        public void setMarker(Marker marker) {
+            this.marker = marker;
+        }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setView(View view) {
+            this.view = view;
+        }
+
+        public boolean isChosen() {
+            return chosen;
+        }
+
+        public void setChosen(boolean chosen) {
+            this.chosen = chosen;
+        }
+    }
 
     public PlacesMapFragment(LatLng latLng, List<Place> places){
         super();
         this.places = places;
         this.selectedLatLng = latLng;
+        placeInfos = new ArrayList<PlaceInfo>();
+        for (Place place: places){
+            placeInfos.add(new PlaceInfo(place));
+        }
     }
 
     public  static PlacesMapFragment newInstance(LatLng latLng, List<Place> places){
@@ -43,11 +97,53 @@ public class PlacesMapFragment extends Fragment implements OnMapReadyCallback, G
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLng(selectedLatLng));
-        for(Place place:places){
-            if(place.getLatLng()!=null) {
-                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName()));//.showInfoWindow();
+        for(PlaceInfo place:placeInfos){
+            if(place.getPlace().getLatLng()!=null) {
+                place.setMarker(mMap.addMarker(new MarkerOptions().position(place.getPlace().getLatLng()).title(place.getPlace().getName())));
             }
         }
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                PlaceInfo placeInfo = null;
+                for(PlaceInfo place: placeInfos){
+                    if (place.getMarker()!=null && place.getMarker().getPosition().equals(marker.getPosition())){
+                        placeInfo = place;
+                        break;
+                    }
+                }
+                if (placeInfo.getView() != null){
+                    return placeInfo.getView();
+                }
+                placeInfo.setView(getActivity().getLayoutInflater().inflate(R.layout.place_marker_layout, null));
+                TextView textView = (TextView) placeInfo.getView().findViewById(R.id.marker_place_name_text_view);
+                textView.setText(marker.getTitle());
+                return placeInfo.getView();
+            }
+        });
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                PlaceInfo placeInfo = null;
+                for(PlaceInfo place: placeInfos){
+                    if (place.getMarker()!=null && place.getMarker().getPosition().equals(marker.getPosition())){
+                        placeInfo = place;
+                        break;
+                    }
+                }
+                CheckBox checkBox = (CheckBox) placeInfo.getView().findViewById(R.id.marker_place_check_box);
+                placeInfo.setChosen(!placeInfo.isChosen());
+                checkBox.setChecked(placeInfo.isChosen());
+                marker.hideInfoWindow();
+                marker.showInfoWindow();
+            }
+        });
     }
 
     @Override
@@ -61,6 +157,7 @@ public class PlacesMapFragment extends Fragment implements OnMapReadyCallback, G
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        return false;
+        marker.showInfoWindow();
+        return true;
     }
 }
